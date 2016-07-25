9978d02b9b4914138bd0fc0d469246689530732c
Nick <hurryup2132@gmail.com>
2016-07-24 14:29:35 -1000

    profile scripts

--- js/app.js
+++ js/app.js
@@ -2,17 +2,81 @@ $(document).foundation();
 
 	var i = 0;
 
-$('#name').click(function() {
-	console.log('click');
+$('.column1').click(function() {
 	i++;
-	console.log(i);
 
 	if (i % 2 === 0) {
-		console.log('even');
 		$("#studInfo").remove();
 	} else {
-		console.log('odd');
+
+	var isOne = $(this).hasClass("column1");
+	var isTwo = $(this).hasClass("column2");
+	var isThree = $(this).hasClass("column3");
+
+	if (isOne === true) {
+		console.log('column1');
+	}
+	if (isTwo === true) {
+		console.log('column2');
+	}
+	if (isThree === true) {
+		console.log('column3');
+	}
+
+	$( "#row1" ).after("<div class=\"callout\" id=\"studInfo\"><h5>This is a callout.</h5><p>It has an easy to override visual style, and is appropriately subdued.</p></div>" );
+
+	}
+});
+
+$('.column2').click(function() {
+	i++;
+
+	if (i % 2 === 0) {
+		$("#studInfo").remove();
+	} else {
+
+		var isOne = $(this).hasClass("column1");
+		var isTwo = $(this).hasClass("column2");
+		var isThree = $(this).hasClass("column3");
+
+		if (isOne === true) {
+			console.log('column1');
+		}
+		if (isTwo === true) {
+			console.log('column2');
+		}
+		if (isThree === true) {
+			console.log('column3');
+		}
+
 		$( "#row1" ).after("<div class=\"callout\" id=\"studInfo\"><h5>This is a callout.</h5><p>It has an easy to override visual style, and is appropriately subdued.</p></div>" );
+
+	}
+});
+
+$('.column3').click(function() {
+	i++;
+
+	if (i % 2 === 0) {
+		$("#studInfo").remove();
+	} else {
+
+		var isOne = $(this).hasClass("column1");
+		var isTwo = $(this).hasClass("column2");
+		var isThree = $(this).hasClass("column3");
+
+		if (isOne === true) {
+			console.log('column1');
+		}
+		if (isTwo === true) {
+			console.log('column2');
+		}
+		if (isThree === true) {
+			console.log('column3');
+		}
+
+		$( "#row1" ).after("<div class=\"callout\" id=\"studInfo\"><h5>This is a callout.</h5><p>It has an easy to override visual style, and is appropriately subdued.</p></div>" );
+
 	}
 });
 
